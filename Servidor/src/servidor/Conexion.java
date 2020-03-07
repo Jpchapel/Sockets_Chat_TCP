466d744952b4d8b061e2f9067b79e3578bd5c542
Jpchapel <jpchapel82@gmail.com>
2020-03-07 18:29:07 +0100

    Refactorizado

--- Servidor/src/servidor/Conexion.java
+++ Servidor/src/servidor/Conexion.java
@@ -8,7 +8,6 @@ package servidor;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
